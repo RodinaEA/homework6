6f789c9dc2f08ea48948f846fca90398e1c5e88c
Elena Rodina <rodina-hr@mail.ru>
2022-12-04 22:09:11 +0500

    task41

---
+++ task41/Program.cs
@@ -0,0 +1,27 @@
+﻿// Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+// 0, 7, 8, -2, -2 -> 2
+// 1, -7, 567, 89, 223-> 3
+
+Console.Write($"Введите размер массива: ");
+int numbers = Convert.ToInt32(Console.ReadLine());
+int[] arrayNumbers = new int[numbers];
+
+void PrintNumbers(int numbers)
+{
+  for (int i = 0; i < numbers; i++)
+  {
+      Console.Write($"Введите число номер {i+1}: ");
+      arrayNumbers[i] = Convert.ToInt32(Console.ReadLine());
+  }
+}
+PrintNumbers(numbers);
+int Sum(int[] arrayNumbers)
+{
+  int count = 0;
+  for (int i = 0; i < arrayNumbers.Length; i++)
+  {
+    if(arrayNumbers[i] > 0 ) count += 1; 
+  }
+  return count;
+}
+Console.WriteLine($"Вы ввели {Sum(arrayNumbers)} чисел больше 0");
